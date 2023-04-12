88f4705878dd14966820badc250138bb92953851
Masahiro Kasahara <masahiro@kasahara.ws>
2023-04-12 19:00:01 +0900

    ADD: initializeDB.js: Check the permission of the config file so we eliminated the security concerns.

--- initializeDB.js
+++ initializeDB.js
@@ -1,7 +1,28 @@
 const { Client } = require("pg");
 const fs = require("fs");
 
-const config = JSON.parse(fs.readFileSync("config.json"));
+const config_file_path = "config.json";
+
+if(!fs.existsSync(config_file_path, fs.constants.F_OK)) {
+  console.log('ERROR: The config file does not exist. Please create a config file \'config.json\'.');
+  process.exit(1);
+}
+
+try {
+  const stats = fs.statSync(config_file_path);
+  const fileMode = stats.mode & 0o777; // convert to octal
+
+  if (fileMode === 0o600) {
+    console.log('The file permission is 600');
+  } else {
+    console.log('ERROR: The file permission of config is not 600. Please change the file permission to 600 for security reasons..');
+    return 1;
+  }
+} catch (err) {
+  console.error(err);
+}
+
+const config = JSON.parse(fs.readFileSync(config_file_path));
 
 const client = new Client({
   user: config.password,
