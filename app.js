4a48142ea67b6d5fd88fdaae319abb503ad260ae
hkamada <kamahirodada@gmail.com>
2023-05-09 17:14:10 +0900

    a

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/",require("./routes"));
+const auth = { username: "admin", password: "admin" };
+app.use((req, res, next) => {
+  const b64auth = req.headers.authorization || "";
+  const [username, password] = Buffer.from(
+    b64auth.split(" ")[1] || "",
+    "base64"
+  )
+    .toString()
+    .split(":");
+  if (
+    username &&
+    password &&
+    username === auth.username &&
+    password === auth.password
+  ) {
+    return next();
+  }
+  res.set("WWW-Authenticate", 'Basic realm="401"');
+  res.status(401).send("Authentication required.");
+});
+
+app.use("/", require("./routes"));
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
-
 
 module.exports = app;
