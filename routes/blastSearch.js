445b3b81b505df9afc56f2fed34055c1b4e26d26
hkamada <kamahirodada@gmail.com>
2023-05-18 14:33:29 +0900

    deleted blastSearch.js

--- routes/blastSearch.js
+++
@@ -1,9 +0,0 @@
-const { search, register, update } = require("./manipulatedb");
-
-const blastSearch = (jobid, injson) => {
-  const ended = new Date();
-  const outjson = "test:result";
-  update(jobid, outjson, ended);
-  return search(jobid);
-};
-module.exports = blastSearch;
