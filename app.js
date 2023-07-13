d8831c3d007b6276c46f3fadb057c444336c9c6e
hkamada <kamahirodada@gmail.com>
2023-07-13 14:07:27 +0900

    localhost5000

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 const allowCrossDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
