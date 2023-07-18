1835435f3190dcd6cca81f77cefc8e9c04554fe2
hkamada <kamahirodada@gmail.com>
2023-07-18 17:52:20 +0900

    3000->5000->8080

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const fs = require("fs");
 const app = express();
+const cors = require("cors");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,17 +17,28 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-const allowCrossDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization,access-token"
+const server_file_path = "server.json";
+if (!fs.existsSync(server_file_path, fs.constants.F_OK)) {
+  console.log(
+    "ERROR: server.json file not found. Please execute initializeenv.js first."
   );
-  next();
-};
-app.use(allowCrossDomain);
+  process.exit(1);
+}
+const serverFile = JSON.parse(fs.readFileSync(server_file_path));
+const reactAppServerUrl = serverFile.reactAppServerUrl;
+
+app.use(cors({ origin: reactAppServerUrl, credentials: true }));
+// const allowCrossDomain = (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept,Authorization,access-token"
+//   );
+//   next();
+// };
+// app.use(allowCrossDomain);
 
 const master_file_path = "master.json";
 if (!fs.existsSync(master_file_path, fs.constants.F_OK)) {
@@ -36,30 +48,24 @@ if (!fs.existsSync(master_file_path, fs.constants.F_OK)) {
   process.exit(1);
 }
 const master = JSON.parse(fs.readFileSync(master_file_path));
-const clientAuth = master.client;
-const executorAuth = master.executor;
-//const auth = { username: "admin", password: "admin" };
+
 app.use((req, res, next) => {
   const b64auth = req.headers.authorization || "";
-  const [username, password] = Buffer.from(
-    b64auth.split(" ")[1] || "",
-    "base64"
-  )
-    .toString()
-    .split(":");
+  const authHeader = req.headers.authorization || "";
+  const authString = authHeader.replace(/^Basic\s+/, "");
+  const decodedAuth = Buffer.from(authString, "base64").toString("utf-8");
+  const [username, password] = decodedAuth.split(":");
   if (
     req.method === "OPTIONS" ||
     (username &&
       password &&
-      ((username === executorAuth.username &&
-        password === executorAuth.password) ||
-        (username === clientAuth.username && password === clientAuth.password)))
+      username === master.username &&
+      password === master.password)
   ) {
     return next();
   } else {
     console.log("Invalid credentials");
     console.log("username: " + username);
-    console.log("password: " + password);
   }
   res.set("WWW-Authenticate", 'Basic realm="401"');
   res.status(401).send("Authentication required.");
