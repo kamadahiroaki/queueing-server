e7c0acea122e4af2c1bf9ea9ea6acf0154c819c1
hkamada <kamahirodada@gmail.com>
2023-11-30 14:50:00 +0900

    fix generateMasterPass.js

--- generateMasterPass.js
+++ generateMasterPass.js
@@ -21,37 +21,11 @@ fs.writeFile("master.json", JSON.stringify(master), (err) => {
     console.log("cannot write master.json" + err);
     return;
   }
-  console.log("master.json is created");
+
+  try {
+    fs.chmodSync("master.json", 0o600);
+    console.log("master.json is created");
+  } catch (err) {
+    console.log("cannot change the permission of master.json" + err);
+  }
 });
-fs.chmodSync("master.json", 0o600);
-
-// const envData =
-//   "REACT_APP_SERVER_URL=http://localhost:5000" +
-//   "\n" +
-//   "QUEUEING_SERVER_URL=http://localhost:8080" +
-//   "\n" +
-//   "MASTER_USERNAME=" +
-//   master.username +
-//   "\n" +
-//   "MASTER_PASSWORD=" +
-//   master.password +
-//   "\n";
-// fs.writeFileSync(".env", envData, (err) => {
-//   if (err) {
-//     console.log("cannot write .env" + err);
-//     return;
-//   }
-//   console.log(".env is created");
-// });
-
-try {
-  fs.chmodSync("master.json", 0o600);
-} catch (err) {
-  console.log("cannot change the permission of master.json" + err);
-}
-
-// try {
-//   fs.chmodSync(".env", 0o600);
-// } catch (err) {
-//   console.log("cannot change the permission of .env" + err);
-// }
